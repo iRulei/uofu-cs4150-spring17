65433edf01329cc04b529c4746355e7b3a7d0f78
iRulei <rule.leon@hotmail.com>
2017-02-10 13:13:12 -0700

    ~

--- Kattis4 - AutoSink/Kattis4 - AutoSink/AutoSink.cs
+++ Kattis4 - AutoSink/Kattis4 - AutoSink/AutoSink.cs
@@ -10,13 +10,26 @@ namespace AutoSink
     {
         static void Main(string[] args)
         {
-            int cityCount;
-            int 
+            int cityCount = -1;
+            int hwCount = -1;
+            int tripCount = -1;
+            int lc = 0;
 
             string line;
             while ((line = Console.ReadLine()) != null)
             {
-
+                if (lc == 0)
+                {
+                    cityCount = Int32.Parse(line);
+                }
+                else if (lc == (cityCount + 1))
+                {
+                    hwCount = Int32.Parse(line);
+                }
+                else if (lc == (cityCount + hwCount + 2))
+                {
+                    tripCount = Int32.Parse(line);
+                }
             }
         }
     }
