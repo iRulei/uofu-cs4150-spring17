bc07cc551364254c6ecbb22bf1ca80638ae96e5b
iRulei <rule.leon@utah.edu>
2017-02-24 14:39:27 -0700

    ~

--- Kattis6 - GetShorty/Kattis6 - GetShorty/Program.cs
+++ Kattis6 - GetShorty/Kattis6 - GetShorty/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,46 @@ namespace GetShorty
     {
         static void Main(string[] args)
         {
+            List<Dungeon> dungeons = new List<Dungeon>();
+
+            //string line = "";
+            //while ((line = Console.ReadLine()) != null)
+            string[] lines = File.ReadAllLines("k6test1.txt");
+            for(int i = 0; i < lines.Count(); i++)
+            {
+                string[] thisLine = lines[i].Split();
+                if (thisLine.Count() == 2)
+                    dungeons.Add(new Dungeon(Int32.Parse(thisLine[0]), Int32.Parse(thisLine[1])));                    
+                else
+                {
+                    dungeons.Last.rooms.
+                }
+            }
+
+            // process each test case
+
+        }
+    }
+
+    public class Dungeon
+    {
+        public Dictionary<string, List<string>> halls;
+
+        public Dungeon(int _rc, int _hc)
+        {
+            halls = new Dictionary<string, List<string>>();
+        }
+
+        public class Hall
+        {
+            public string end;
+            public int    wgt;
+
+            public Hall(string _end, int _wgt)
+            {
+                end = _end;
+                wgt = _wgt;
+            }
         }
     }
 }
